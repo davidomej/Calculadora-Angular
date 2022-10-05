adce725e901a20eb95e4778ee6c8a10b15f15e50
David Domínguez Mejías <davidomej@hotmail.com>
2022-10-05 21:39:16 +0100

    Calculadora
    Primer commit, calculadora funcionando la suma

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FootComponent } from './components/foot/foot.component';
+import { CalculateComponent } from './components/calculate/calculate.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FootComponent,
+    CalculateComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/calculate/calculate.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calculate',
+  templateUrl: './calculate.component.html',
+  styleUrls: ['./calculate.component.css']
+})
+export class CalculateComponent implements OnInit {
+
+  public op1:number=0;
+  public op2:number=0;
+
+  public result:number=0;
+
+  constructor() { }
+
+
+  calculate():number{
+    this.result=this.op1+this.op2
+    return this.result
+  }
+
+  ngOnInit(): void {
+  }
+
+}
